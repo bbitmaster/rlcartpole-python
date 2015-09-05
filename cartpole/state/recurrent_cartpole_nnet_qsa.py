0e9c32da9b6a8d5597cd532af543155790923f8e
Ben Goodrich <bbitmaster@gmail.com>
2015-09-05 16:15:56 -0400

    added extra network layer support

--- cartpole/state/recurrent_cartpole_nnet_qsa.py
+++ cartpole/state/recurrent_cartpole_nnet_qsa.py
@@ -42,6 +42,14 @@ class recurrent_cartpole_nnet_qsa(object):
                                  initialization_constant=p['initialization_constant'],
                                  dropout=p['dropout'],use_float32=p['use_float32'],
                                  momentum=p['momentum'],maxnorm=p['maxnorm'],step_size=p['learning_rate']))
+        if(p.has_key('num_hidden2') and p['num_hidden2'] is not None):
+            layers.append(nnet.layer(p['num_hidden2'],p['activation_function2'],
+                                 initialization_scheme=p['initialization_scheme2'],
+                                 initialization_constant=p['initialization_constant2'],
+                                 dropout=p['dropout2'],use_float32=p['use_float32'],
+                                 momentum=p['momentum2'],maxnorm=p['maxnorm2'],step_size=p['learning_rate2']))
+ 
+
         layers.append(nnet.layer(1,
                                  initialization_scheme=p['initialization_scheme_final'],
                                  initialization_constant=p['initialization_constant_final'],
@@ -50,8 +58,8 @@ class recurrent_cartpole_nnet_qsa(object):
 
         self.net = nnet.net(layers)
 
-        self.do_neuron_clustering=False #by default
         if(p.has_key('cluster_func') and p['cluster_func'] is not None):
+            print("layer 0 has cluster func")
             self.cluster_func = p['cluster_func']
             #TODO: Make sure the centroids cover the input space appropriately
             self.net.layer[0].centroids = np.asarray(((np.random.random((self.net.layer[0].weights.shape)) - 0.5) * 2.25),np.float32)
@@ -63,7 +71,6 @@ class recurrent_cartpole_nnet_qsa(object):
             #print('cluster_func: ' + str(csf.select_names[p['cluster_func']]))
             self.net.layer[0].centroid_speed = p['cluster_speed']
             self.net.layer[0].num_selected = p['clusters_selected']
-            self.do_neuron_clustering=True #set a flag to log neurons that were used for clustering
             if(p.has_key('do_cosinedistance') and p['do_cosinedistance']):
                 self.net.layer[0].do_cosinedistance = True
                 print('cosine set to true')
@@ -72,11 +79,45 @@ class recurrent_cartpole_nnet_qsa(object):
                 self.net.layer[0].zeta = 1.0
                 self.zeta_decay = p['zeta_decay']
 
+
+        if(p.has_key('cluster_func2') and p['cluster_func2'] is not None):
+            print("layer 1 has cluster func")
+            self.cluster_func2 = p['cluster_func2']
+            #TODO: Make sure the centroids cover the input space appropriately
+            self.net.layer[1].centroids = np.asarray(((np.random.random((self.net.layer[1].weights.shape)) - 0.5) * 2.25),np.float32)
+            #make the centroid bias input match the bias data of 1.0
+            self.net.layer[1].centroids[:,-1] = 1.0
+            #print(str(self.net.layer[0].centroids.shape))
+            #print(str(self.net.layer[0].centroids))
+            self.net.layer[1].select_func = csf.select_names[p['cluster_func2']]
+            #print('cluster_func: ' + str(csf.select_names[p['cluster_func']]))
+            self.net.layer[1].centroid_speed = p['cluster_speed2']
+            self.net.layer[1].num_selected = p['clusters_selected2']
+            if(p.has_key('do_cosinedistance') and p['do_cosinedistance']):
+                self.net.layer[1].do_cosinedistance = True
+                print('cosine set to true')
+            if(p.has_key('zeta_decay2') and p['zeta_decay2'] is not None):
+                self.net.layer[1].zeta_matrix = np.ones(self.net.layer[1].weights.shape,dtype=np.float32)
+                self.net.layer[1].zeta = 1.0
+                self.zeta_decay = p['zeta_decay2']
+
+
+
         if(p.has_key('_lambda') and p['_lambda'] is not None):
             self._lambda = p['_lambda']
             self.gamma = p['gamma']
             for l in self.net.layer:
                 l.eligibility = l.gradient
+
+
+        print("Network Size Check:" )
+        print("  weight 0 size: " + str(self.net.layer[0].weights.shape))
+        if(len(self.net.layer) > 1):
+            print("  weight 1 size: " + str(self.net.layer[1].weights.shape))
+        if(len(self.net.layer) > 2):
+            print("  weight 2 size: " + str(self.net.layer[2].weights.shape))
+        if(len(self.net.layer) > 3):
+            print("  weight 3 size: " + str(self.net.layer[3].weights.shape))
 
 
     def store(self,state,action,value):
@@ -111,6 +152,15 @@ class recurrent_cartpole_nnet_qsa(object):
             self.net.update_weights()
             #move centroids
             csf.update_names[self.cluster_func](self.net.layer[0])
+        elif(hasattr(self.net.layer[1],'zeta')):
+        #decay zeta
+            self.net.layer[1].zeta = self.net.layer[1].zeta*self.zeta_decay
+            self.net.layer[1].zeta_matrix[:] = self.net.layer[1].zeta
+            self.net.layer[1].step_size = (1.0 - self.net.layer[1].zeta_matrix)*self.learning_rate
+
+            self.net.update_weights()
+            #move centroids
+            csf.update_names[self.cluster_func2](self.net.layer[1])
         else:
             self.net.update_weights()
 
---
+++ params/cartpole_nn_clusterfunc_hyperopt_2layer_params.py
@@ -0,0 +1,132 @@
+from math import exp, log
+#cartpole_default_params
+runtype='sarsa'
+
+#A handy name for this run. The data file will be given this name as
+#<resultsdir><simname><version>.h5py
+simname = 'cartpole_sarsa_test'
+version = '1.1'
+results_dir = '../results/'
+
+#if load_name is set, then the simulation will load this file and resume from there, this is useful for watching the behavior of a trained agent
+#load_name = '../results/cartpole_sarsa_test1.1.h5py'
+
+data_dir = '../data/'
+save_interval = 30*60
+
+#run for a total number of episodes
+train_episodes=20000
+max_steps=1000
+
+use_float32=True
+
+random_seed = 4;
+
+save_images=False
+image_save_dir="/home/bgoodric/tmp2/" #I Guess that underutilized windows partitition with all that storage is good for something...
+
+
+#how far to bound each state variable before the simulation is considered invalid
+vel_bound = 15;
+angle_vel_bound = 15;
+pos_bound = 10
+
+#storage types are 'tabular' and 'nnet'
+qsa_type='cluster_nnet'
+
+#parameters for neural network qsa
+activation_function='tanh'
+
+num_hidden=32
+learning_rate = 0.01
+learning_rate_decay_type='geometric'
+learning_rate_decay=1.0
+learning_rate_min=0.00001
+momentum=0.0
+maxnorm=None
+dropout=None
+
+initialization_scheme='glorot'
+initialization_scheme_final='glorot'
+
+initialization_constant=1.0
+initialization_constant_final=1.0
+
+#cluster_func stuff
+cluster_func = None
+clusters_selected=0
+cluster_speed=0.0
+
+activation_function2='linear'
+
+num_hidden2=2048
+learning_rate2 = 0.01
+momentum2=0.0
+maxnorm2=None
+dropout2=None
+
+initialization_scheme2='glorot'
+initialization_scheme_final2='glorot'
+
+initialization_constant2=1.0
+initialization_constant_final2=1.0
+
+#cluster_func stuff
+cluster_func2 = 'cluster_func'
+clusters_selected2=32
+cluster_speed2=0.0
+
+
+#action is encoded using one hot encoding with these as the "hot" and "not hot" targets
+incorrect_target = -1.0
+correct_target = 1.0
+
+#cart-pole hyperparemeters
+g=9.81  #gravity
+l=0.5   #pole length
+mp=0.01 #pole mass
+mc=1.0  #cart mass
+
+#delta-t, how much time to pass between frame steps
+dt=0.02
+
+#how hard to push left and right
+push_force = 10.0
+
+#reinforcement learning parameters
+negative_reward = -1.0
+positive_reward = 0.01
+no_reward = 0.0
+
+#decay_type can be 'geometric' or 'linear'
+decay_type='geometric'
+epsilon=0.05
+epsilon_min=0.005
+#epsilon_decay=exp((log(epsilon_min) - log(epsilon))/10000.0)
+#epsilon_decay = (epsilon - epsilon_min)/15000
+epsilon_decay=0.9997
+gamma=0.99
+
+action_type='e_greedy'
+#action_type='noisy_qsa'
+qsa_avg_alpha = 0.99
+qsa_avg_init  = 0.0
+
+#If defined, will print the state variables on every frame
+print_state_debug=True
+
+#in sarsa mode, this tells if the SDL display should be enabled. Set to False if the machine does not have pygame installed
+do_vis=False
+
+#in sarsa mode, this tells how often to display, -1 for none
+showevery=500
+
+#these affect the display. They tell the size in pixels of the display, the axis size, and how many frames to skip
+display_width=1280
+display_height=720
+axis_x_min=-10.0
+axis_x_max=10.0
+axis_y_min=-5.5
+axis_y_max=5.5
+fps=60
+
